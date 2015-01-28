071d301ab8006d94138f380ed8ff08232e197eda
Chris Viqueira <chrisviqueira@gmail.com>
2015-01-28 12:00:28 -0500

    Initial Commit of the project with the test scene

---
+++ Marballs (Unity)/Assets/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public Transform ball;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.LookAt (ball);
+	}
+}

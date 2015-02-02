012aaab144cfd28ea59f6c094050246a6edb6d57
TheSquishyDitto <squishyditto@yahoo.com>
2015-02-02 16:47:02 -0500

    Camera and marble controls fixed and implemented.

--- MarballsUnity/Assets/Scripts/CameraController.cs
+++ MarballsUnity/Assets/Scripts/CameraController.cs
@@ -1,51 +1,125 @@
-﻿using UnityEngine;
+﻿/// <summary>
+/// CameraController.cs
+/// Authors: Kyle Dawson, Chris Viqueira, [ANYONE ELSE WHO MODIFIES CODE PUT YOUR NAME HERE]
+/// Date Created:  Jan. 28, 2015
+/// Last Revision: Feb.  2, 2015
+/// 
+/// Class that controls camera movement.
+/// 
+/// NOTES: - Current camera controls are C, and either arrow keys or mouse depending on mode.
+/// 
+/// TO DO: - Tweak movement until desired.
+/// 
+/// </summary>
+
+using UnityEngine;
 using System.Collections;
 
 public class CameraController : MonoBehaviour {
-		
-	public float theta;  // degrees around y-axis
-	public float psy;	 // degrees around x-axis
-	public float radius; // distance from ball
 
-	public Transform ball; // get the ball coordinates
+	// Enum for determining how to control camera.
+	public enum ControlMode {
+		Keyboard,
+		Mouse
+	}
 
-	// Use this for initialization
+	// Variables
+	// (Regions don't do anything functionally, they just help organize code and can be collapsed)
+	#region Variables
+	public Transform ball; // Reference to coordinates of marble
+
+	public float theta;  // Radians around y-axis (horizontal).
+	public float psy;	 // Radians around x-axis (vertical).
+	public float radius; // Distance from marble.
+
+	public const float PSYMAX = (Mathf.PI / 2) - 0.1f; // Maximum value for psy. Camera inverts at Pi/2+.
+	public const float PSYMIN = 0;					   // Minimum value for psy.
+
+	public float smooth = 1; // Used to adjust how smoothly the camera adjusts position between frames (Lerp)
+
+	public ControlMode mode;		 // Which control mode camera is using.
+	public float sensitivity = 0.2f; // Mouse sensitivity.
+
+	#endregion
+
+	// Start - Use this for initialization
 	void Start () {
-		/*theta = 0f;
-		psy = 45f;*/
-		radius = .001f;
+		ball = GameObject.FindGameObjectWithTag("Marble").transform;
+		radius = Vector3.Distance(transform.position, ball.position);
+		mode = ControlMode.Keyboard;
 	}
 	
-	// Update is called once per frame
+	// Update - Called once per frame
 	void Update () {
-		if (Input.GetKey (KeyCode.UpArrow)) {
-			psy += .01f;		
+		// Keyboard mode controls
+		if (mode == ControlMode.Keyboard) { 
+			if (Input.GetKey (KeyCode.UpArrow)) {
+				psy = Mathf.Clamp(psy + .01f, PSYMIN, PSYMAX);
+			}
+
+			if (Input.GetKey (KeyCode.DownArrow)) {
+				psy = Mathf.Clamp(psy - .01f, PSYMIN, PSYMAX);
+			}
+
+			if (Input.GetKey (KeyCode.LeftArrow)) {
+				theta -= .01f;
+			}
+
+			if (Input.GetKey (KeyCode.RightArrow)) {
+				theta += .01f;	
+			}
+
+		// Mouse mode controls
+		} else if (mode == ControlMode.Mouse) {
+			if (Input.GetMouseButton(0)) { // 0 is left mouse button.
+				Screen.lockCursor = true; // While true, cursor is hidden and constantly put in middle of screen.
+				psy = Mathf.Clamp(psy + (Input.GetAxis("Mouse Y") * sensitivity), PSYMIN, PSYMAX);
+				theta += Input.GetAxis("Mouse X") * sensitivity;
+			}
+
+			if (Input.GetMouseButtonUp(0)) { // 0 is left mouse button.
+				Screen.lockCursor = false;
+			}
+
+			radius -= Input.GetAxis("Mouse ScrollWheel");
+		
+		// Unspecified case controls
+		} else {
+				Debug.LogWarning("(CameraController.cs) Control mode not specified!");
 		}
-		if (Input.GetKey (KeyCode.DownArrow)) {
-			psy -= .01f;		
+
+		if (Input.GetKeyDown(KeyCode.C)) {
+			ToggleControlMode();
 		}
-		if (Input.GetKey (KeyCode.LeftArrow)) {
-			theta -= .01f;
-		}
-		if (Input.GetKey (KeyCode.RightArrow)) {
-			theta += .01f;	
-		}
-					
+
 	}
 
+	// LateUpdate - Called directly after everything updates
 	void LateUpdate () {
-		gameObject.transform.position = getSphericalPosition ();
+		// Attempted to use Lerp to fix mild jitter, but couldn't get it to work.
+		//gameObject.transform.position = Vector3.Lerp(transform.position, GetSphericalPosition(), Time.deltaTime * smooth);
+		gameObject.transform.position = GetSphericalPosition();
+		gameObject.transform.LookAt(ball.position);
 	}
 
 
-	// Return spherical coordinate of camera
-	public Vector3 getSphericalPosition() {
+	// GetSphericalPosition - Return spherical coordinate of camera
+	Vector3 GetSphericalPosition() {
 		Vector3 retPos = new Vector3();
 
-		retPos.x = radius * (Mathf.Cos (psy) * Mathf.Rad2Deg) * (Mathf.Cos (theta) * Mathf.Rad2Deg) + ball.position.x;
-		retPos.y = radius * (Mathf.Sin (psy)  * Mathf.Rad2Deg)  + ball.position.y;
-		retPos.z = radius * (Mathf.Cos (psy) * Mathf.Rad2Deg) * (Mathf.Sin (theta) * Mathf.Rad2Deg)  + ball.position.z;
+		// Turns out radians were needed after all.
+		retPos.x = radius * Mathf.Cos (psy) * Mathf.Cos (theta) + ball.position.x;
+		retPos.y = radius * Mathf.Sin (psy) + ball.position.y;
+		retPos.z = radius * Mathf.Cos (psy) * Mathf.Sin (theta) + ball.position.z;
 
 		return retPos;
+	}
+
+	// ToggleControlMode - Changes camera control style.
+	void ToggleControlMode() {
+		if (mode == ControlMode.Keyboard)
+			mode = ControlMode.Mouse;
+		else if (mode == ControlMode.Mouse)
+			mode = ControlMode.Keyboard;
 	}
 }
--- MarballsUnity/Assets/Scripts/Marble.cs
+++ MarballsUnity/Assets/Scripts/Marble.cs
@@ -1,13 +1,15 @@
 ﻿/// <summary>
 /// Marble.cs
-/// Authors: Kyle Dawson, [ANYONE ELSE WHO MODIFIES CODE PUT YOUR NAME HERE]
+/// Authors: Kyle Dawson, Chris Viqueira, [ANYONE ELSE WHO MODIFIES CODE PUT YOUR NAME HERE]
 /// Date Created:  Jan. 28, 2015
-/// Last Revision: Jan. 28, 2015
+/// Last Revision: Feb.  2, 2015
 /// 
 /// Class that controls marble properties and actions.
 /// 
-/// TO DO:
-/// 	- Tweak movement until desired.
+/// NOTES: - Current controls for marble are WASD, Space, and B.
+/// 
+/// TO DO: - Tweak movement until desired.
+/// 	   - Detect when ball is in air to prevent multijumping.
 /// 
 /// </summary>
 
@@ -16,38 +18,58 @@ using System.Collections;
 
 public class Marble : MonoBehaviour {
 	// Variables
-	public float speedMultiplier = 10000f;	// How speedy the variety of marble should be.
+	public Transform cam;				// Reference to the main camera.
+
+	Vector3 inputDirection;					// Holds desired direction of input before applying it.
+	public float speedMultiplier;			// How speedy the variety of marble should be. Changes must be large to be noticeable.
 	// public float weight;					// Weight may be nice for messing with gravity.
 
 
 	// Start - Use this for initialization
 	void Start () {
-	
+		inputDirection = new Vector3();
+		cam = GameObject.FindGameObjectWithTag("MainCamera").transform;
 	}
 	
 	// Update - Called once per frame
 	void Update () {
-		// Currently, all of these controls are with respect to the world.
-		// These should soon be changed to be with respect to the camera. Yay vector math.
 
+		inputDirection = Vector3.zero; // Clears direction so force doesn't accumulate even faster.
+
+		// Forward.
 		if (Input.GetKey (KeyCode.W)) {
-			this.gameObject.rigidbody.AddTorque (Vector3.forward * speedMultiplier, ForceMode.Acceleration);
+			inputDirection += cam.forward;
 		}
 
+		// Backward.
 		if (Input.GetKey (KeyCode.S)) {
-			this.gameObject.rigidbody.AddTorque (Vector3.back * speedMultiplier, ForceMode.Acceleration);
+			inputDirection -= cam.forward;
 		}
 
+		// Left.
 		if (Input.GetKey (KeyCode.A)) {
-			this.gameObject.rigidbody.AddTorque (Vector3.left * speedMultiplier, ForceMode.Acceleration);
+			inputDirection -= cam.right;
 		}
 
+		// Right.
 		if (Input.GetKey (KeyCode.D)) {
-			this.gameObject.rigidbody.AddTorque (Vector3.right * speedMultiplier, ForceMode.Acceleration);
+			inputDirection += cam.right;
 		}
 
+		inputDirection.y = 0; // Removes vertical component from camera vectors.
+		inputDirection = Vector3.Normalize(inputDirection); // Makes sure the magnitude of the direction is 1.
+
+		rigidbody.AddForce(inputDirection * speedMultiplier * Time.deltaTime); // Applies force.
+		//rigidbody.AddTorque(inputDirection * speedMultiplier * Time.deltaTime); // Can also try this, but I haven't.
+
+		// Jump.
 		if (Input.GetKeyDown (KeyCode.Space)) {
-			this.gameObject.rigidbody.AddForce (0f, 200f, 0f);
+			rigidbody.AddForce (0, 200, 0);
+		}
+
+		// Very basic, extremely potent brake. Can currently pause ball midair for the most part.
+		if (Input.GetKey (KeyCode.B)) {
+			rigidbody.velocity = Vector3.zero;
 		}
 	}
 }

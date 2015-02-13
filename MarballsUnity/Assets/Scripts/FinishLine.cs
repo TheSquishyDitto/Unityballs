bafb2a82e98441d512049e1dd7ed71841a1e224e
sunworld23 <sunworld23@gmail.com>
2015-02-13 09:53:47 -0500

    Preliminary level 1

---
+++ MarballsUnity/Assets/Scripts/FinishLine.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FinishLine : MonoBehaviour {
+
+	public GameMaster gm;
+
+	void Awake () {
+		gm = GameMaster.CreateGM ();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	// When player gets to finish
+	void OnTriggerEnter () {
+		gm.OnWin();
+		Debug.Log("You win");
+	}
+}

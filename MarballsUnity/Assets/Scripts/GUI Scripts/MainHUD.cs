5bf41e7958b55c79089b244d655035c2f3ea4b53
sunworld23 <sunworld23@gmail.com>
2015-03-11 12:13:11 -0400

    New main HUD

---
+++ MarballsUnity/Assets/Scripts/GUI Scripts/MainHUD.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MainHUD : MonoBehaviour {
+	public GameMaster gm;	// Reference to Game Master.
+	public Transform marble;	// Reference to currently active marble.
+	public Text timer;
+	public Text speed;
+	
+	// Awake - Called before anything else.
+	void Awake () {
+		gm = GameMaster.CreateGM();
+	}
+	
+	// Use this for initialization
+	void Start () {
+		marble = gm.marble;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!gm.paused)
+			timer.text = (Mathf.Round(gm.timer * 10) / 10.0) + " s";
+		
+		if (marble != null) {
+			// Speed gauge.
+			speed.text = Mathf.Round(marble.GetComponent<Rigidbody>().velocity.magnitude) + " m/s";
+		}	
+	}
+	
+	// Debug buttons
+	public void StartButton (){
+		gm.OnStart();
+	}
+	
+	public void PlayButton (){
+		gm.OnPlay();
+	}
+}

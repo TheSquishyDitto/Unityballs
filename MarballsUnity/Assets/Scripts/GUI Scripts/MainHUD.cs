1525b79ec33c80e293fab68226c77a446c7dbb59
sunworld23 <sunworld23@gmail.com>
2015-03-12 18:38:59 -0400

    Finalized Demo

--- MarballsUnity/Assets/Scripts/GUI Scripts/MainHUD.cs
+++ MarballsUnity/Assets/Scripts/GUI Scripts/MainHUD.cs
@@ -17,7 +17,7 @@ using UnityEngine.UI;
 
 public class MainHUD : MonoBehaviour {
 
-	// Variables
+		// Variables
 	#region Variables
 	public GameMaster gm;		// Reference to Game Master.
 	public Transform marble;	// Reference to currently active marble.
@@ -27,11 +27,16 @@ public class MainHUD : MonoBehaviour {
 	public Image powerup;		// Reference to powerup picture.
 	public Image deathScreen;	// Reference to death tint.
 	public Text deathMessage;	// Reference to death message text.
+	public Image winScreen;		// Reference to win tint.
+	public Text winMessage;		// Reference to win message text.
+	
+	public GameObject winOptions; // Reference to win options.
 	public GameObject debugSet;	// Reference to debug buttons and info display.
 
 	public Sprite[] nums;		// Easy holder of number textures.
 	float remainder;			// Decimal portion of timer.
 	public float goLength;		// Desired duration of "GO!" Should be between 0 and 1.
+	int nextLevel;				// Stores value of next level
 	#endregion
 	
 	// Awake - Called before anything else.
@@ -49,6 +54,7 @@ public class MainHUD : MonoBehaviour {
 		gm.OnStart();	// Begins the gameplay sequence when the HUD is ready.
 
 		debugSet.SetActive(gm.debug);
+		nextLevel = Application.loadedLevel + 1;
 	}
 	
 	// Update is called once per frame
@@ -89,6 +95,19 @@ public class MainHUD : MonoBehaviour {
 	public void PlayButton (){
 		gm.OnPlay();
 	}
+	
+	public void Restart (){
+		gm.LoadLevel(Application.loadedLevel);
+	}
+	
+	public void LevelSelect (){
+		gm.levelSelect = true;
+		gm.LoadLevel(0);
+	}
+	
+	public void NextLevel (){
+		gm.LoadLevel(nextLevel);
+	}
 
 	// OnDeath - Called when player falls off the stage or otherwise is killed.
 	public IEnumerator OnDeath() {
@@ -112,5 +131,30 @@ public class MainHUD : MonoBehaviour {
 		deathMessage.color = new Color(1, 1, 1, 0);
 
 		gm.marble.GetComponent<Marble>().Respawn(); // Finally respawns player.
+	}
+	
+	public IEnumerator OnVictory() {
+		
+		// Makes the red screen visible.
+		for (int i = 0; i < 25; i++) {
+			winScreen.color = new Color(winScreen.color.r, winScreen.color.g, winScreen.color.b, i/50.0f);
+			yield return new WaitForSeconds(0.05f);
+		}
+		
+		// Makes the death text visible.
+		for (int i = 0; i < 10; i++) {
+			winMessage.color = new Color(winMessage.color.r, winMessage.color.g, winMessage.color.b, i/10.0f);
+			yield return new WaitForSeconds(0.05f);
+		}
+		
+		yield return new WaitForSeconds(0.75f);
+		
+		GameObject nextLevelButton = winOptions.transform.FindChild("Next Level").gameObject;		
+		if(nextLevel >= Application.levelCount){
+			nextLevelButton.GetComponent<Button>().interactable = false;
+		}
+		winOptions.SetActive(true);
+		
+				
 	}
 }

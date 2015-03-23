0cdded0d813bb0f9aa7c06f5353ddeded2118cb4
sunworld23 <sunworld23@gmail.com>
2015-03-23 09:54:30 -0400

    Finish line camera

---
+++ MarballsUnity/Assets/Scripts/FinishArrow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FinishArrow : MonoBehaviour {
+	public GameMaster gm;
+
+	void Awake () {
+		gm = GameMaster.CreateGM();	// Refers to Game Master, see GameMaster code for details.
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = gm.cam.rotation;
+		transform.position = gm.cam.position;
+	}
+}

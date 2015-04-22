e11d5c3d02220242893514faec72616366aaf274
sunworld23 <sunworld23@gmail.com>
2015-04-22 09:50:57 -0400

    Level select edits

--- MarballsUnity/Assets/Scripts/GUI Scripts/LevelButton.cs
+++ MarballsUnity/Assets/Scripts/GUI Scripts/LevelButton.cs
@@ -5,7 +5,7 @@ using System.Collections;
 public class LevelButton : MonoBehaviour {
 
 	public LevelDataObject levelData;	// Data of level to pull from.
-	public int buildLevel = 0;			// Index of which level to load.
+	// public int buildLevel = 0;			// Index of which level to load.
 	public Text highScore;				// Reference to high score text.
 	// NOTE: To get high scores dynamically, we will likely need to standardize the level data naming conventions for added levels.
 	// 		 ex. Level1, Level2, Level3, Level4, Level5, just so we can load them even without having a reference.
@@ -14,13 +14,20 @@ public class LevelButton : MonoBehaviour {
 
 	// Start - Use this for initialization.
 	void Start () {
-		self = GetComponent<Button>();
-		self.onClick.AddListener(ButtonLoadLevel);
+		//self = GetComponent<Button>();
+		//self.onClick.AddListener(ButtonLoadLevel);
 		if (levelData != null) highScore.text = "High Score: " + levelData.bestTimes[0].ToString("F2") + " s";
 	}
-
+	
+	/*
 	void ButtonLoadLevel() {
 		//Debug.Log("It worked!");
 		Application.LoadLevel(buildLevel);
 	}
+	*/
+	
+	public void LoadLevel(int level){
+		Application.LoadLevel(level);
+	}
+	
 }
